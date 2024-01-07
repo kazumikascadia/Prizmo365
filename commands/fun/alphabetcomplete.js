74474393848de756b4b61a6de460d173343c996f
Chloe-Woahie <68732833+fekie@users.noreply.github.com>
2024-01-06 19:27:32 -0500

    chore: refactor input filtering

--- commands/fun/alphabetcomplete.js
+++ commands/fun/alphabetcomplete.js
@@ -1,81 +1,108 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+// We create the list of allowed characters. These are only
+// in uppercase which means that characters must be converted to uppercase.
+const allowedCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
+// Accepts the input from an interaction and returns a sorted
+// and filtered output.
+function filterInput(input) {
+    return input
+        .toUpperCase()
+        .split("")
+        .sort()
+        .filter((c) => {
+            return allowedCharacters.includes(c);
+        });
+}
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('alphabet')
-		.setDescription('Tells the amount of letters in an alphabet.')
-		.addStringOption(option =>
-			option.setName('input')
-				.setDescription('The word/sentence you want to put in.')
-				.setRequired(true)
-				.setMaxLength(1000)),
-	async execute(interaction) {
-		// sets up all necessary constants for the embed
-		const iUser = interaction.user;
-		const nickname = iUser.nickname ?? iUser.displayName;
-		const avatar = iUser.displayAvatarURL();
-		// gathers input to be used
-		const input = interaction.options.getString('input');
-		// creates an array using the input
-		const array = input.toUpperCase().split('');
-		// creates the real letters array
-		const realLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-		// creates a usable array for the undesirable characters and a second for the output
-		const undesirables = [];
-		const output = [];
-		// set up the embed with starting variables
-		const aEmbed = new EmbedBuilder()
-			.setAuthor({ name: nickname, iconURL: avatar })
-			.setTimestamp(+new Date());
+    data: new SlashCommandBuilder()
+        .setName("alphabet")
+        .setDescription("Tells the amount of letters in an alphabet.")
+        .addStringOption((option) =>
+            option
+                .setName("input")
+                .setDescription("The word/sentence you want to put in.")
+                .setRequired(true)
+                .setMaxLength(1000)
+        ),
+    async execute(interaction) {
+        // sets up all necessary constants for the embed
+        const iUser = interaction.user;
+        const nickname = iUser.nickname ?? iUser.displayName;
+        const avatar = iUser.displayAvatarURL();
 
-		// sorts the array
-		array.sort();
+        // set up the embed with starting variables
+        const aEmbed = new EmbedBuilder()
+            .setAuthor({ name: nickname, iconURL: avatar })
+            .setTimestamp(+new Date());
 
-		// removes undesirable characters from the array
-		array.forEach(element => {
-			if (!realLetters.includes(element)) {
-				undesirables.push(element);
-			}
-		});
-		// pushes unused characters into an output array
-		array.forEach(element => {
-			if (!output.includes(element) && !undesirables.includes(element)) {
-				output.push(element);
-			}
-			else {
-				return;
-			}
-		});
+        // Gather and filter the input.
+        const input = interaction.options.getString('input');
+        const filteredInput = filterInput(input);
 
-		// check if the output is actually greater than one; if it isn't, it means that there will be no output, which cannot be accepted
-		// if the output is greater than one, continue as normal
-		if (output.length > 1) {
-			// turns the length of the output array into a constant
-			const letters = output.length;
-			// formats the list into a proper sentence
-			const fOutput = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
-			const fOutput2 = fOutput.format(output);
-			// creates the final embed
-			aEmbed
-				.setTitle('Your word, split apart.')
-				.setDescription(`Your word or sentence,\n> ${input}\ncontains the following letters: \n> ${fOutput2}. \nOverall, it contains ${letters}/26 letters.`)
-				.setColor('Green');
+        // check if the output is actually greater than one; if it isn't, it means that there will be no output, which cannot be accepted
+        // if the output is greater than one, continue as normal
+        if (filteredInput.length > 1) {
+            // turns the length of the output array into a constant
+            const letters = filteredInput.length;
+            // formats the list into a proper sentence
+            const fOutput = new Intl.ListFormat("en", {
+                style: "long",
+                type: "conjunction",
+            });
+            const fOutput2 = fOutput.format(filteredInput);
+            // creates the final embed
+            aEmbed
+                .setTitle("Your word, split apart.")
+                .setDescription(
+                    `Your word or sentence,\n> ${input}\ncontains the following letters: \n> ${fOutput2}. \nOverall, it contains ${letters}/26 letters.`
+                )
+                .setColor("Green");
 
-			// replies to the interaction with the embed
-			interaction.reply({ embeds: [aEmbed] });
-		}
-		// in all other cases, send an error
-		else {
-			// creates the final embed, telling the user what is wrong
-			aEmbed
-				.setTitle('Error')
-				.setDescription('Because you put no actual letters into your statement, none can be counted.')
-				.setColor('Red');
+            // replies to the interaction with the embed
+            interaction.reply({ embeds: [aEmbed] });
+        }
+        // in all other cases, send an error
+        else {
+            // creates the final embed, telling the user what is wrong
+            aEmbed
+                .setTitle("Error")
+                .setDescription(
+                    "Because you put no actual letters into your statement, none can be counted."
+                )
+                .setColor("Red");
 
-			// sends the message
-			interaction.reply({ embeds: [aEmbed] });
-
-		}
-
-	},
+            // sends the message
+            interaction.reply({ embeds: [aEmbed] });
+        }
+    },
 };
